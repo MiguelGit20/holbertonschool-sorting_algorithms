750e85cfc4634be9aab9405eceff497235bdb3f1
MiguelGit20 <5178@holbertonschool.com>
2022-09-08 22:19:26 -0700

    function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,54 +1,45 @@
 #include "sort.h"
 /**
- *sortedInsert - insert node
- *@list: double linked list
- *@newNode: node
- *Return: void
- */
-void sortedInsert(listint_t **list, listint_t *newNode)
-{
-	listint_t *current;
-
-	if (*list == NULL)
-		*list = newNode;
-	else if ((*list)->n >= newNode->n)
-	{
-		newNode->next = *list;
-		newNode->next->prev = newNode;
-		*list = newNode;
-	}
-	else
-	{
-		current = *list;
-		while (current->next != NULL && current->next->n < newNode->n)
-			current = current->next;
-		newNode->next = current->next;
-		if (current->next != NULL)
-			newNode->next->prev = newNode;
-		current->next = newNode;
-		newNode->prev = current;
-	}
-}
-/**
- *insertion_sort_list - sorts the list
+ *insertion_sort_list - sorts a doubly linked list in ascending order
  *@list: double linked list
  *Return: void
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *sorted = NULL;
-	listint_t *current = *list;
-	listint_t *next = NULL;
+	listint_t *current, *prevN, *nextN;
 
 	if (!list)
 		return;
 
+	current = (*list)->next;
+
 	while (current != NULL)
 	{
-		next = current->next;
-		current->prev = current->next = NULL;
-		sortedInsert(&sorted, current);
-		current = next;
+		if (current->prev != NULL)
+		{
+			if (current->n < current->prev->n)
+			{
+				prevN = current->prev;
+				nextN = current->next;
+				current->prev = prevN->prev;
+				current->next = prevN;
+				if (current->prev != NULL)
+					current->prev->next = current;
+
+				prevN->prev = current;
+				prevN->next = nextN;
+				if (nextN != NULL)
+					nextN->prev = prevN;
+				if (current->prev == NULL)
+					*list = current;
+
+				print_list(*list);
+			}
+			else
+				current = current->next;
+		}
+		else
+			current = current->next;
 	}
-	*list = sorted;
+
 }
